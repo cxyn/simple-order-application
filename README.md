924315c91f8ed8e752b3b288f2186329c1f63c46
colin <i@wangyunxiao.com>
2019-02-13 23:16:44 +0800

    initialize simple order application

---
+++ README.md
@@ -0,0 +1,26 @@
+# Simple Order Application
+
+> 一个小点餐应用，采用Element UI && vue-cli
+
+## Start
+
+ - 克隆仓库到本地
+ - 进入你的本地文件夹，安装依赖，执行:
+
+``` bash
+yarn
+```
+
+## Develop
+
+``` bash
+# 启动热重载服务，url: http://localhost:8010
+npm run dev
+```
+
+## Build
+
+``` bash
+# 压缩混淆代码，构建项目用于生产环境
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+    <h2>今日菜单</h2>
+    <el-table
+    ref="multipleTable"
+    :data="menuList"
+    tooltip-effect="dark"
+    style="width: 100%"
+    @selection-change="handleSelectionChange">
+
+    <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+
+    <el-table-column
+      label="套餐"
+      width="120">
+      <template slot-scope="scope">{{ scope.row.name }}</template>
+    </el-table-column>
+
+    <el-table-column
+      prop="price"
+      label="价格"
+      width="120">
+    </el-table-column>
+
+    <el-table-column
+      prop="num"
+      label="数量"
+      show-overflow-tooltip>
+      <template slot-scope="scope">
+        <el-input-number v-model="scope.row.num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+      </template>
+    </el-table-column>
+
+  </el-table>
+  <div style="margin-top: 20px">
+    <el-button type="primary" round @click="submitOrder()">提交订单</el-button>
+  </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        menuList: [],
+        multipleSelection: []
+      }
+    },
+    mounted: function() {
+      let _this = this;
+      let _arr = [
+        {
+            name: '鱼香肉丝',
+            price: '13',
+            num: '1'
+          },
+          {
+            name: '素菜',
+            price: '13',
+            num: '1'
+          },
+          {
+            name: '京酱肉丝',
+            price: '13',
+            num: '1'
+          },
+          {
+            name: '口水鸡',
+            price: '13',
+            num: '1'
+          }
+      ]
+      _arr.forEach((item)=>{
+        _this.menuList.push(item)
+      })
+    },
+    methods: {
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      handleChange(value) {
+        console.log(value)
+      },
+      submitOrder() {
+        console.log(this.multipleSelection)
+        if(this.multipleSelection.length === 0) {
+          this.$message({
+            showClose: true,
+            message: '请选择套餐！',
+            type: 'warning'
+          })
+        }else {
+          console.log('发起请求')
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+.el-table th>.cell {
+  text-align: center;
+}
+</style>
