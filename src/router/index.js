94b8cf0a16060c4a8ed6c902ef441655e8912640
colin <i@wangyunxiao.com>
2019-02-14 23:25:53 +0800

    re-initialize project by vue-cli && add router mapping

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import TodayMenu from '@/components/TodayMenu'
+import OrderListToday from '@/components/OrderListToday'
+import SignUp from '@/components/SignUp'
+import SignIn from '@/components/SignIn'
+import AdminIndex from '@/components/AdminIndex'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'TodayMenu',
+      component: TodayMenu
+    },
+    {
+      path: '/orderListToday',
+      name: 'orderListToday',
+      component: OrderListToday
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn
+    },
+    {
+      path: '/admin/index',
+      name: 'adminIndex',
+      component: AdminIndex
+    }
+
+  ]
+})
